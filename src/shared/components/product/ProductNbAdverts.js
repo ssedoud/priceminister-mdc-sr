ccc35bd557a1defcaf0d2cf0d056e021f65bee7f
Lucie <lilougarden@gmail.com>
2016-03-04 11:32:38 +0100

    Fix nb adverts display on product list

--- src/shared/components/product/ProductNbAdverts.js
+++ src/shared/components/product/ProductNbAdverts.js
@@ -4,12 +4,12 @@ class ProductNbAdverts extends React.Component {
   render() {
     if (this.props.nbAdverts > 0) {
       return <span><span className="offerSummary">
-          {this.props.nbAdverts} {this.props.advertType === "new" ? 'neufs' : 'occasions'} dès
+          {this.props.nbAdverts} {this.props.advertType === "new" ? 'neuf' : 'occasion'}{this.props.nbAdverts > 1 ? 's' : ''} dès
           </span>
           <a href ="#">
             <span className="smallPrice"> {this.props.bestPrice} € </span>
           </a>
-          &nbsp;&nbsp;&nbsp;&nbsp;</span>;
+          </span>;
     }
     else {
       return <span></span>;
