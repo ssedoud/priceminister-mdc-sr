b4526260d52b75480f1c5d54adbe6ff4f2294ee3
marionanfreville <marionanfreville@club-internet.fr>
2016-03-04 11:40:20 +0100

    Ajustement css et ontenu placeholder ProductSellYoursComponent

--- src/shared/components/product/ProductSellYoursComponent.js
+++ src/shared/components/product/ProductSellYoursComponent.js
@@ -11,11 +11,13 @@ class ProductSellYoursComponent extends React.Component {
                <div className="confiancePM align-right">
                  <span className="category ">Livraison garantie par Priceminister et Service clients à votre écoutes <a href="#">(?)</a></span>
                </div>
-               <div className="col-xs-4 input-group input-group-sm">
-                 <span className="input-group-btn">
-                   <button className="btn btn-default bkColorPM" type="button">Vendez le vôtre</button>
-                 </span>
-                 <input type="text" className="form-control" placeholder="35€" />
+               <div className="col-md-2">
+                 <div className="input-group input-group-sm">
+                   <span className="input-group-btn">
+                     <button className="btn btn-default bkColorPM" type="button">Vendez le vôtre</button>
+                   </span>
+                   <input type="text" className="form-control" placeholder={this.props.product.usedBestPrice} />
+                 </div>
                </div>
              </div>
            </div>;
